4bd6318a3f7efbf3977e5e6749c7030ab24fb40b
Rodrigo Martins <rodrigo.ifsemg@gmail.com>
2021-06-06 17:56:49 -0300

    First commit

---
+++ src/main/java/br/com/PersonAPIKobaldo/PersonAPIKobaldo/KobaldosPersonApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.PersonAPIKobaldo.PersonAPIKobaldo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KobaldosPersonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KobaldosPersonApiApplication.class, args);
+	}
+
+}
